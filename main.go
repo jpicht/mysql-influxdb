de249d5e29979ac081aa91c0b3f7adff5a98a1c6
Julian Picht <julian.picht@gmail.com>
2017-05-03 13:08:30 +0200

    tracking more stuff

--- main.go
+++ main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	_ "github.com/go-sql-driver/mysql"
-	influx "github.com/influxdata/influxdb/client/v2"
-	"github.com/jmoiron/sqlx"
-	"github.com/jpicht/mysql-influxdb/influxsender"
 	"io/ioutil"
 	"os"
 	"os/signal"
 	"regexp"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
+
+	"github.com/ftloc/exception"
+	_ "github.com/go-sql-driver/mysql"
+	influx "github.com/influxdata/influxdb/client/v2"
+	"github.com/jmoiron/sqlx"
+	"github.com/jpicht/logger"
+	"github.com/jpicht/mysql-influxdb/influxsender"
 )
 
 type (
@@ -32,10 +36,30 @@ type (
 		Name  string `db:"Variable_name"`
 		Value string `db:"Value"`
 	}
+	ProcesslistAbbrevLine struct {
+		Command  string `db:"COMMAND"`
+		Database string `db:"DB"`
+		Host     string `db:"REMOTE"`
+		State    string `db:"STATE"`
+		Count    int    `db:"COUNT"`
+	}
+	EventWaitsHostsLine struct {
+		Host    string  `db:"HOST"`
+		Event   string  `db:"EVENT_NAME"`
+		Count   int64   `db:"COUNT_STAR"`
+		SumWait float64 `db:"SUM_TIMER_WAIT"`
+		MinWait float64 `db:"MIN_TIMER_WAIT"`
+		AvgWait float64 `db:"AVG_TIMER_WAIT"`
+		MaxWait float64 `db:"MAX_TIMER_WAIT"`
+	}
 	RunningHostInfo struct {
 		Name       string
 		Connection *sqlx.DB
 		Tags       map[string]string
+	}
+	TmpPoint struct {
+		tags   map[string]string
+		values map[string]interface{}
 	}
 )
 
@@ -50,6 +74,35 @@ func failOnError(err error, f string, data ...interface{}) {
 	}
 	fmt.Fprintf(os.Stderr, f+"\n", data...)
 	os.Exit(1)
+}
+
+func newTmpPointProcList(tags map[string]string, command string, db string, host string, state string) *TmpPoint {
+	copy := make(map[string]string)
+	for k, v := range tags {
+		copy[k] = v
+	}
+	copy["command"] = command
+	copy["db"] = db
+	copy["client"] = host
+	copy["state"] = state
+	return &TmpPoint{
+		tags:   copy,
+		values: make(map[string]interface{}),
+	}
+}
+
+func newTmpPoint(tags map[string]string, additional map[string]string, values map[string]interface{}) *TmpPoint {
+	copy := make(map[string]string)
+	for k, v := range tags {
+		copy[k] = v
+	}
+	for k, v := range additional {
+		copy[k] = v
+	}
+	return &TmpPoint{
+		tags:   copy,
+		values: values,
+	}
 }
 
 func (c *Config) UnmarshalJSON(data []byte) error {
@@ -72,6 +125,21 @@ func (c *Config) UnmarshalJSON(data []byte) error {
 	}
 	c.Interval = tmp_duration
 	return nil
+}
+
+func send(log logger.Logger, now time.Time, sender influxsender.InfluxSender, measurement string, tags map[string]string, values map[string]interface{}) {
+	p, err := influx.NewPoint(
+		measurement,
+		tags,
+		values,
+		now,
+	)
+
+	if err == nil {
+		sender.AddPoint(p)
+	} else {
+		log.WithData("error", err).Warning("Error creating point")
+	}
 }
 
 func main() {
@@ -120,19 +188,27 @@ func main() {
 
 	tick := time.NewTicker(interval)
 	sendTick := time.NewTicker(c.Influx.Interval)
+	log := logger.NewStdoutLogger()
 
+	failed := 0
 	for {
 		select {
 		case <-tick.C:
 			wg := sync.WaitGroup{}
-			wg.Add(len(cons))
+			wg.Add(2 * len(cons))
 
 			for i := range cons {
-				go func() {
+				now := time.Now()
+				now_ts := now.Unix()
+
+				go func(now time.Time) {
 					defer wg.Done()
 					rows := make([]Line, 0)
 					err = cons[i].Connection.Select(&rows, "SHOW GLOBAL STATUS")
-					failOnError(err, "Error: %s", err)
+					if err != nil {
+						log.WithData("error", err).Warning("MySQL-Query-Error")
+						return
+					}
 
 					values := map[string]interface{}{}
 
@@ -150,13 +226,111 @@ func main() {
 						"mysql",
 						cons[i].Tags,
 						values,
-						time.Now(),
+						now,
 					)
 
-					failOnError(err, "Error: %s", err)
+					if err == nil {
+						sender.AddPoint(p)
+					} else {
+						log.WithData("error", err).Warning("Error creating point")
+					}
+				}(now)
 
-					sender.AddPoint(p)
-				}()
+				go func(now time.Time) {
+					defer wg.Done()
+					rows := make([]ProcesslistAbbrevLine, 0)
+					err = cons[i].Connection.Select(
+						&rows,
+						"SELECT "+
+							"IFNULL(COMMAND, '') COMMAND, "+
+							"IFNULL(DB, '') DB, "+
+							"SUBSTRING_INDEX(HOST, ':', 1) AS REMOTE, "+
+							"IFNULL(STATE, '') STATE, "+
+							"COUNT(*) AS `COUNT` "+
+							"FROM PROCESSLIST "+
+							"WHERE USER NOT IN ('system user', 'repl') "+
+							"GROUP BY COMMAND, DB, REMOTE, STATE;",
+					)
+					if err != nil {
+						log.WithData("error", err).Warning("MySQL-Query-Error")
+						return
+					}
+
+					tmp := map[string]*TmpPoint{}
+
+					for _, row := range rows {
+						state := strings.Replace(row.State, " ", "_", -1)
+						if state == "" {
+							state = row.Command
+						}
+
+						key := row.Command + row.Database + row.Host + row.State
+						if _, ok := tmp[key]; !ok {
+							tmp[key] = newTmpPointProcList(cons[i].Tags, row.Command, row.Database, row.Host, state)
+						}
+
+						tmp[key].values["threads"] = row.Count
+					}
+
+					for _, tmpP := range tmp {
+						p, err := influx.NewPoint(
+							"threads",
+							tmpP.tags,
+							tmpP.values,
+							now,
+						)
+
+						if err == nil {
+							sender.AddPoint(p)
+						} else {
+							log.WithData("error", err).Warning("Error creating point")
+						}
+					}
+				}(now)
+
+				/*
+					go func(now time.Time) {
+						defer wg.Done()
+						rows := make([]EventWaitsHostsLine, 0)
+						err = cons[i].Connection.Select(
+							&rows,
+							"SELECT * FROM events_waits_summary_by_host_by_event_name WHERE COUNT_STAR > 0;",
+						)
+						if err != nil {
+							log.WithData("error", err).Warning("MySQL-Query-Error")
+							return
+						}
+
+						for _, row := range rows {
+							tmpP := newTmpPoint(
+								cons[i].Tags,
+								map[string]string{
+									"client": row.Host,
+									"event":  row.Event,
+								},
+								map[string]interface{}{
+									"count":    row.Count,
+									"sum_wait": row.SumWait,
+									"min_wait": row.MinWait,
+									"max_wait": row.MaxWait,
+									"avg_wait": row.AvgWait,
+								},
+							)
+							p, err := influx.NewPoint(
+								"waits",
+								tmpP.tags,
+								tmpP.values,
+								now,
+							)
+
+							if err == nil {
+								sender.AddPoint(p)
+							} else {
+								log.WithData("error", err).Warning("Error creating point")
+							}
+						}
+					}(now)
+				*/
 			}
 			wg.Wait()
 		case <-sendTick.C:
